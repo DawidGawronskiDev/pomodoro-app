6605ea43ed4a05abfabef98c8fc30e1e12fd10a2
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-07-09 23:02:37 +0100

    Create Link

---
+++ src/components/Link.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import { useTimerContext, type Timer } from "../store/TimerContextProvider";
+
+type LinkProps = {
+  timer: Timer;
+  index: number;
+};
+
+export default function Link({ timer, index }: LinkProps) {
+  const { currentTimerIndex, handleCurrentTimerIndex } = useTimerContext();
+
+  return (
+    <li>
+      <button
+        onClick={() => handleCurrentTimerIndex(index)}
+        className={clsx("px-6 py-4 rounded-full", {
+          "bg-c-100": currentTimerIndex === index,
+          "text-c-400": currentTimerIndex !== index,
+        })}
+      >
+        <p className="body-one">{timer.name}</p>
+      </button>
+    </li>
+  );
+}
