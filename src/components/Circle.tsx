94c8f1729f2c64dd5ce722a1ae012427fc0139a4
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-07-09 23:21:50 +0100

    Update Circle

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -15,6 +15,8 @@ export default function Circle({
   const circ = 2 * Math.PI * radius;
   const strokePct = ((100 - percentage) * circ) / 100;
 
+  console.log(strokePct);
+
   return (
     <div className="-rotate-90">
       <svg height={size} width={size} xmlns="http://www.w3.org/2000/svg">
@@ -26,7 +28,7 @@ export default function Circle({
           stroke={color}
           strokeWidth={stroke}
           strokeDasharray={circ}
-          strokeDashoffset={strokePct}
+          strokeDashoffset={percentage === 0 ? 0 : strokePct}
           strokeLinecap="round"
           className="transition-all ease-linear"
         />
